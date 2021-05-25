6a4a8e4f66949ccfe008aa93bde055ddbb863ae2
Luis Felipe <luisfelipe_2511@hotmail.com>
2021-05-24 19:10:19 -0500

    Ya esta funcional el codigo, se agrego el arreglo para guardar los consultorios y ya funciona, , falta modificar unas cosas en el metodo agregarCita() pero funciona. Corregir la impresion del metodo imprimir(); imprime el texto cola vacia y no deberia

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -83,16 +83,16 @@ public class Principal {
 
      */
     public static void agregarMedicos() {
-        consultorios[0] = new Consultorio(new Doctor("Luis", 25));
-        consultorios[1] = new Consultorio(new Doctor("Fer", 11));
-        consultorios[2] = new Consultorio(new Doctor("Jose", 28));
-        consultorios[3] = new Consultorio(new Doctor("Raul", 32));
-        consultorios[4] = new Consultorio(new Doctor("Raul", 32));
-        consultorios[5] = new Consultorio(new Doctor("Raul", 32));
-        consultorios[6] = new Consultorio(new Doctor("Raul", 32));
-        consultorios[7] = new Consultorio(new Doctor("Raul", 32));
-        consultorios[8] = new Consultorio(new Doctor("Raul", 32));
-        consultorios[9] = new Consultorio(new Doctor("Raul", 32));
+        consultorios[0] = new Consultorio(new Doctor("Luis", 1));
+        consultorios[1] = new Consultorio(new Doctor("Fer", 2));
+        consultorios[2] = new Consultorio(new Doctor("Jose", 3));
+        consultorios[3] = new Consultorio(new Doctor("Raul", 4));
+        consultorios[4] = new Consultorio(new Doctor("Raul", 5));
+        consultorios[5] = new Consultorio(new Doctor("Raul", 6));
+        consultorios[6] = new Consultorio(new Doctor("Raul", 7));
+        consultorios[7] = new Consultorio(new Doctor("Raul", 8));
+        consultorios[8] = new Consultorio(new Doctor("Raul", 9));
+        consultorios[9] = new Consultorio(new Doctor("Raul", 10));
 
     }
 
